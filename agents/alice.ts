a8ea74308d4cfcf4d621589b2fe123106498a684
Fattakhov Emil <emil.e.fattakhov@gmail.com>
2023-06-29 13:58:26 -0500

    Add a yaml integration example

--- agents/alice.ts
+++ agents/alice.ts
@@ -1,23 +1,34 @@
 import { IConfig } from "../interfaces.ts";
 import { Sapial } from "../sapial.ts";
+import fs from 'node:fs';
+import YAML from 'yaml';
+
 
 // we need to create a single fastAPI server
 // we may spawn multiple models
 
+const alice_config = YAML.parse(fs.readFileSync('./config.yml', 'utf8'))
 const config: IConfig = {
-    name: "Alice",
-    primaryModel: "gpt-4",
-    secondaryModel: "gpt-4",
-    memory: true,
+    name: alice_config.name,
+    primaryModel: alice_config.primaryModel,
+    secondaryModel: alice_config.secondaryModel,
+    memory: alice_config.memory,
 }
 
-const alice = Sapial.init(config);
+const alice = await Sapial.init(config);
 
 console.log("Alice is online");
 
 // const prompt = "Hello, I am Alice. What is your name?"
-// const response = await alice.call(prompt);
+// const response = await alice.chatLLM(prompt);
 // console.log(response);
+
+
+// call(prompt)
+// embed(document)
+// memory
+// tools
+// spawn
 
 
 
